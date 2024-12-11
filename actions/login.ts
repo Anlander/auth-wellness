1efefcd1ad630f585f2d31308ed0c130db331d7a
anlander <anlanderp@gmail.com>
2024-12-11 16:18:37 +0100

    init prisma

---
+++ actions/login.ts
@@ -0,0 +1,13 @@
+"use server";
+
+import * as z from "zod";
+import { LoginSchema } from "@/schemas";
+
+export const login = async (values: z.infer<typeof LoginSchema>) => {
+  const valitedFields = LoginSchema.safeParse(values);
+  if (!valitedFields.success) {
+    return { error: "Invalid fields!" };
+  }
+
+  return { success: "Valid fields, Loading.." };
+};
---
+++ actions/register.ts
@@ -0,0 +1,13 @@
+"use server";
+
+import * as z from "zod";
+import { RegisterSchema } from "@/schemas";
+
+export const register = async (values: z.infer<typeof RegisterSchema>) => {
+  const valitedFields = RegisterSchema.safeParse(values);
+  if (!valitedFields.success) {
+    return { error: "Invalid fields!" };
+  }
+
+  return { success: "Account created!" };
+};
